24cd9bb10bd2e0b9afd802eabc9e0ac9829da57e
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-04-04 19:00:37 +0200

    use a common makefile for rules

---
+++ common.mk
@@ -0,0 +1,16 @@
+# Raise error if the stack name is not set
+ifndef STACK_NAME
+$(error STACK_NAME is not set)
+endif
+
+include ../config.mk
+
+all: deploy
+
+.PHONY: deploy
+deploy:
+	DOCKER_CONTEXT=${DOCKER_CONTEXT} docker stack deploy --detach=true --compose-file compose.yml --prune ${STACK_NAME}
+
+.PHONY: deploy
+undeploy:
+	DOCKER_CONTEXT=${DOCKER_CONTEXT} docker stack rm ${STACK_NAME}
--- mirrors/config.mk
+++
@@ -1,1 +0,0 @@
-STACK_NAME ?= mirrors
