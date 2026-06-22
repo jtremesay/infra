8dd5dcaa91cd3284eaf9c0c7d3919fe9c415029b
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-06-22 19:41:24 +0200

    enable fwupd service in music configuration

--- machines/music/configuration.nix
+++ machines/music/configuration.nix
@@ -22,6 +22,8 @@
     ../../users
   ];
 
+  services.fwupd.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
